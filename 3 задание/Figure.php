2506032781da33da497df2d77357111e71d92ee9
Aleksei <aleksei_kopkin@mail.ru>
2019-03-19 11:29:59 +0300

    Main file

--- 3 задание/Figure.php
+++ 3 задание/Figure.php
@@ -1,74 +1,4 @@
 <?php
-$object = '';
-$file = file('file.txt');
-$action = 'random';
-$randomNumber = 0;
-$randomNumber1 = 0;
-$randomNumber2 = 0;
-if ($action == 'random') {
-    $figure[] = array();
-    $figure[0] = 'circle';
-    $figure[1] = 'square';
-    $figure[2] = 'pyramid';
-    $randomFigure = rand(0,2);
-    switch ($figure[$randomFigure]) {
-        case 'circle':
-            $randomNumber = rand();
-            $object[] = new Circle($randomNumber);
-            file_put_contents('file.txt', PHP_EOL . ($figure[$randomFigure].','.$randomNumber), FILE_APPEND);
-            break;
-        case 'square':
-            $randomNumber = rand();
-            $object[] = new Square($randomNumber);
-            file_put_contents('file.txt', PHP_EOL . ($figure[$randomFigure].','.$randomNumber), FILE_APPEND);
-            break;
-        case 'pyramid':
-            $randomNumber1 = rand();
-            $randomNumber2 = rand();
-            $object[] = new Pyramid($randomNumber1,$randomNumber2);
-            file_put_contents('file.txt', PHP_EOL . ($figure[$randomFigure].','.$randomNumber1.','.$randomNumber2), FILE_APPEND);
-            break;
-    }
-} else {
-    foreach ($file as $val){
-        $who = explode(',', trim($val)); 
-        switch ($who[0]){
-            case 'circle':
-                $object[] = new Circle($who[1]);
-                break;
-            case 'square':
-                $object[] = new Square($who[1]);
-                break;
-            case 'pyramid':
-                $object[] = new Pyramid($who[1], $who[2]);
-                break;
-        }
-    }
-}
-if ($object) {
-    $i=0;
-    $array[]=new Data();
-    foreach ($object as $v) {
-        $array[$i]=new stdClass();
-        $array[$i]->type=$v->getType();
-        $array[$i]->area=$v->getArea();
-        $i=$i+1;
-    }
-    uasort($array,"mySort");
-    print_r($array);
-}
-
-function mySort($f1,$f2)
-{
-    if($f1->area < $f2->area) {
-        return 1;
-    } elseif($f1->area > $f2->area) {
-        return -1;
-    } else {
-        return 0;
-    }
-}
-
 class Data
 {
     public $type = "";
@@ -92,6 +22,7 @@ class Figure
     }
 }
 
+//Класс квадрат
 class Square extends Figure
 {
     private $side;
@@ -114,6 +45,7 @@ class Square extends Figure
     }
 }
 
+//Класс пирамида
 class Pyramid extends Figure
 {
 private $perimeter;
@@ -139,6 +71,7 @@ private $apophem;
     }
 }
 
+//Класс круг
 class Circle extends Figure
 {
     private $radius;
